feea690bbe54c0d5ff9ce823a145e5c854b45a89
Eric van Riet Paap <ericvrp@gmail.com>
2021-05-02 14:55:56 +0200

    Add .env

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,13 @@
 use iota::Client;
 use tokio::time::{sleep, Duration};
-use dotenv::dotenv;
-use std::env;
+
+#[macro_use]
+extern crate dotenv_codegen;
 
 #[tokio::main]
 async fn main() {
+    println!("{}", dotenv!("IOTA_SEED"));
+
     loop {
         let iota = Client::builder() // Crate a client instance builder
             .with_node("https://chrysalis-nodes.iota.org") // "https://api.lb-0.testnet.chrysalis2.com")
@@ -14,7 +17,7 @@ async fn main() {
             .unwrap();
 
         let info = iota.get_info().await.unwrap();
-        println!("{} {:#?}", env::var("IOTA_SEED"), info);
+        println!("{:#?}", info);
 
         sleep(Duration::from_millis(10 * 1000)).await;
     }
