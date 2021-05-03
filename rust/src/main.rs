2560e33cee19f0f3856b476fa8da1a0481a29473
Eric van Riet Paap <ericvrp@gmail.com>
2021-05-03 11:53:08 +0200

    my own BC030 spammer :-)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,7 +9,9 @@ async fn main() {
     // let seed = dotenv!("IOTA_SEED");
     // println!("{}", seed);
 
-    // loop {
+    let message_index = "BC030";
+    let iterations = 1000000;
+
     let iota = Client::builder() // Crate a client instance builder
         .with_node("https://chrysalis-nodes.iota.org") // "https://api.lb-0.testnet.chrysalis2.com")
         .unwrap()
@@ -18,23 +20,41 @@ async fn main() {
         .unwrap();
 
     let info = iota.get_info().await.unwrap();
-    println!("{:#?}", info);
+    println!("messages_per_second {}", info.nodeinfo.messages_per_second);
+    // println!("{:#?}", info);
 
-    println!("Sending message...");
-    let message = iota
-        .message()
-        .with_index("BC030")
-        .with_data("forever young".as_bytes().to_vec())
-        .finish()
-        .await
-        .unwrap();
-    // println!("Message {:#?}", message);
-    // println!("Message id {:#?}", message.id());
+    println!("Send {} messages", iterations);
+
     println!(
-        "https://explorer.iota.org/mainnet/message/{}",
-        message.id().0
+        "https://explorer.iota.org/mainnet/indexed/{}",
+        message_index
     );
 
-    // sleep(Duration::from_millis(10 * 1000)).await;
-    // }
+    for iteration in 1..=iterations {
+        let message = iota
+            .message()
+            .with_index(message_index)
+            .with_data("forever young".as_bytes().to_vec())
+            .finish()
+            .await
+            .unwrap();
+        // println!("Message {:#?}", message);
+        // println!("Message id {:#?}", message.id());
+        println!(
+            "https://explorer.iota.org/mainnet/message/{}",
+            message.id().0
+        );
+
+        if iteration % 10 == 0 {
+            let info = iota.get_info().await.unwrap();
+            println!("messages_per_second {}", info.nodeinfo.messages_per_second);
+        }
+
+        // sleep(Duration::from_millis(10 * 1000)).await;
+    }
+
+    println!(
+        "https://explorer.iota.org/mainnet/indexed/{}",
+        message_index
+    );
 }
